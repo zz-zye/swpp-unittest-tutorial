7b44a22bfa2adb170ea3ee1347f80bd0f5fdd2e6
Changjin Jeong <creatively.jeong@gmail.com>
2021-09-26 11:18:47 +0900

    [Checkpoint 4] Test components with external dependencies

---
+++ src/store/actions/todo.test.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+import * as actionCreators from './todo';
+import store from '../store';
+
+describe('ActionCreators', () => {
+  // Implementation using `jest.fn` API
+  it(`'getTodo' should fetch todos correctly`, (done) => {
+    const stubTodoList = [{
+      id: 0,
+      title: 'title 1',
+      content: 'content 1'
+    }, ];
+
+    // Replace axios.get with mock
+    axios.get = jest.fn(url => {
+      return new Promise((resolve, reject) => {
+        const result = {
+          status: 200,
+          data: stubTodoList
+        };
+        resolve(result);
+      })
+    });
+
+    store.dispatch(actionCreators.getTodos()).then(() => {
+      const newState = store.getState();
+      expect(newState.td.todos).toBe(stubTodoList);
+      expect(axios.get).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+});
