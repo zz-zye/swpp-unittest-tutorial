4df8a4c2e014a56f0d5fd0a76d05783a9c543e5f
Changjin Jeong <creatively.jeong@gmail.com>
2021-09-26 11:19:47 +0900

    [Checkpoint 5] Test components with external dependencies using async/await keywords

--- src/store/actions/todo.test.js
+++ src/store/actions/todo.test.js
@@ -4,6 +4,10 @@ import * as actionCreators from './todo';
 import store from '../store';
 
 describe('ActionCreators', () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  })
+
   // Implementation using `jest.fn` API
   it(`'getTodo' should fetch todos correctly`, (done) => {
     const stubTodoList = [{
@@ -27,6 +31,33 @@ describe('ActionCreators', () => {
       const newState = store.getState();
       expect(newState.td.todos).toBe(stubTodoList);
       expect(axios.get).toHaveBeenCalledTimes(1);
+      done();
+    });
+  });
+
+  // Implementation using `spyOn` API
+  it(`'getTodo' should fetch todos correctly`, (done) => {
+    const stubTodoList = [{
+      id: 0,
+      title: 'title 1',
+      content: 'content 1'
+    }, ];
+
+    const spy = jest.spyOn(axios, 'get')
+      .mockImplementation(url => {
+        return new Promise((resolve, reject) => {
+          const result = {
+            status: 200,
+            data: stubTodoList
+          };
+          resolve(result);
+        });
+      })
+
+    store.dispatch(actionCreators.getTodos()).then(() => {
+      const newState = store.getState();
+      expect(newState.td.todos).toBe(stubTodoList);
+      expect(spy).toHaveBeenCalledTimes(1);
       done();
     });
   });
